6730928fad12d662032cee59f19e40f0385a5cd8
Sahilraj3107 <123757250+Sahilraj3107@users.noreply.github.com>
2024-03-07 21:33:13 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,17 +5,7 @@ public class Main {
    
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-       int t =sc.nextInt();
-
-       while(t-->0){
-       int n=sc.nextInt();
-       String s =sc.next();
-       String r="";
-      for(int i=s.length()-1;i>=0;i--)r=r+s.charAt(i);
-       
-     if(s.compareTo(r)<=0)System.out.println(s);
-     else System.out.println(r+s);
-       }
+       System.out.println("Hello World");
        sc.close();
     }
 }
