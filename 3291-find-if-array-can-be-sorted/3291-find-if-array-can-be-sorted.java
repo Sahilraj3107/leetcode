b5c75cae1e90dd48a942f72a3bb74573706f188b
Sahil Raj <123757250+Sahilraj3107@users.noreply.github.com>
2024-11-06 10:26:28 +0530

    Time: 1 ms (100%), Space: 44.4 MB (35.22%) - LeetHub

--- 3291-find-if-array-can-be-sorted/3291-find-if-array-can-be-sorted.java
+++ 3291-find-if-array-can-be-sorted/3291-find-if-array-can-be-sorted.java
@@ -1,8 +1,6 @@
 class Solution {
     private void swap(int[] nums, int j) {
-        int temp = nums[j];
-        nums[j] = nums[j + 1];
-        nums[j + 1] = temp;
+     
     }
 
     private int countSetBit(int num) {
@@ -12,18 +10,29 @@ class Solution {
     public boolean canSortArray(int[] nums) {
         int n = nums.length;
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < (n - i - 1); j++) {
+            for (int j = 0; j < (n -1); j++) {
                 if (nums[j] > nums[j + 1]) {
                     int num1 = countSetBit(nums[j]);
                     int num2 = countSetBit(nums[j + 1]);
                     if (num1 != num2)
                         return false;
-                    swap(nums, j);
+                     int temp = nums[j];
+                     nums[j] = nums[j + 1];
+                     nums[j + 1] = temp;
                 }
             }
-        }
 
+          for(int i=(n-1);i>0;i--){
+            if(nums[i]<nums[i-1]){
+                 int num1 = countSetBit(nums[i]);
+                    int num2 = countSetBit(nums[i - 1]);
+                    if (num1 != num2)
+                        return false;
+                       int temp = nums[i];
+                       nums[i] = nums[i - 1];
+                       nums[i - 1] = temp;
+                }
+            }  
         return true;
 
     }
