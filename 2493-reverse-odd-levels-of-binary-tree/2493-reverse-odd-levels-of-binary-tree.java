31e783c4bb6c532f4eb25f8ce9c31827a04243db
Sahil Raj <123757250+Sahilraj3107@users.noreply.github.com>
2024-12-20 19:45:09 +0530

    Time: 1 ms (72.88%), Space: 48.2 MB (68.82%) - LeetHub

--- 2493-reverse-odd-levels-of-binary-tree/2493-reverse-odd-levels-of-binary-tree.java
+++ 2493-reverse-odd-levels-of-binary-tree/2493-reverse-odd-levels-of-binary-tree.java
@@ -1,34 +1,38 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
 class Solution {
-    public void solve(TreeNode left, TreeNode right,boolean flag){
-        if(left==null || right==null)return;
-         if(flag){
-           left.val =left.val^right.val;
-            right.val =left.val^right.val;
-            left.val =left.val^right.val;
-         }
-          flag =!flag;
-         solve(left.left, right.right, flag);
-         solve(left.right, right.left, flag);
+    public void solve(TreeNode left, TreeNode right, boolean flag, TreeNode root) {
+        if (left == null || right == null)
+            return;
+        if (flag) {
+            left.val = left.val ^ right.val;
+            right.val = left.val ^ right.val;
+            left.val = left.val ^ right.val;
+        }
+        flag = !flag;
+        solve(left.left, right.right, flag,root);
+        if(left!=root)
+        solve(left.right, right.left, flag, root);
     }
+
     public TreeNode reverseOddLevels(TreeNode root) {
-        if(root==null)return null;
-        
-        solve(root.left,root.right,true);
+        if (root == null)
+            return null;
+
+        solve(root, root, false, root);
         return root;
 
     }
