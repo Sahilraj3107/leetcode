0cca5f6a71c6b8946b6e153d08728bc3d0c2e7ea
Sahil Raj <123757250+Sahilraj3107@users.noreply.github.com>
2026-09-05 13:46:42 +0530

    Time: 144 ms (81.36%), Space: 31.9 MB (79.55%) - LeetHub

---
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.py
@@ -0,0 +1,20 @@
+class Solution:
+    def firstStableIndex(self, nums: list[int], k: int) -> int:
+        n = len(nums)
+        arr1 =[0]*n
+        arr2 =[0]*n
+        maxi = -1
+        mini = 1e9
+
+        for i in range(n-1, -1,-1):
+            arr2[i]= min(mini, nums[i])
+            mini = arr2[i]
+
+        for i in range(n):
+            arr1[i] = max(maxi, nums[i])
+            maxi = arr1[i]
+            
+            if (arr1[i]-arr2[i])<=k:
+                return i
+
+        return -1        
